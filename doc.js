9f69a3f5da0dca037ad6329481eefa89cb82830d
Onyi (Dre) Anozie <88848554+onyianozie@users.noreply.github.com>
2023-11-06 22:35:56 -0500

    Your commit message

---
+++ doc.js
@@ -0,0 +1,10 @@
+
+// console.dir(document);
+
+// console.log(document.body)
+// console.log(document.title)
+// document.title = "Dre's Technology CV";
+
+// console.log(document.all)
+
+// console.log(docucment.all[16])
